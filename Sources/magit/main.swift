90d24f085dfcaab76c30724a93c4028a863f8b89
Bendik Solheim <hello@bendik.dev>
2020-04-08 19:34:59 +0200

    Initial GUI for status view

--- Sources/magit/main.swift
+++ Sources/magit/main.swift
@@ -1,67 +1,109 @@
 import Darwin
 import Ashen
+import os.log
+import BowEffects
 
-public class Cmd: Command {
-    
-    public typealias Runner = () -> String
-    public typealias OnResult = (String) -> AnyMessage
-    
-    let runner: Runner
-    let onResult: OnResult
-    
-    public init(runner: @escaping Runner, onResult: @escaping OnResult) {
-        self.runner = runner
-        self.onResult = onResult
-    }
-    
-    static func run(runner: @escaping Runner, onResult: @escaping OnResult) -> Cmd {
-        Cmd(runner: runner, onResult: onResult)
-    }
-    
-    public func start(_ send: @escaping (AnyMessage) -> Void) {
-        let result = self.runner()
-        let message = self.onResult(result)
-        send(message)
-    }
-    
-    
-}
+//public class Cmd: Command {
+//    
+//    public typealias OnResult = (String) -> AnyMessage
+//    
+//    let process: ProcessDescription
+//    let onResult: OnResult
+//    
+//    public init(process: ProcessDescription, onResult: @escaping OnResult) {
+//        self.process = process
+//        self.onResult = onResult
+//    }
+//    
+//    static func run(process: ProcessDescription, onResult: @escaping OnResult) -> Cmd {
+//        Cmd(process: process, onResult: onResult)
+//    }
+//    
+//    public func start(_ send: @escaping (AnyMessage) -> Void) {
+//        let task = execute(process: self.process)
+//        let result = task.unsafeRunSyncEither(on: .global(qos: .background))
+//        let branch = result.fold({ error in error.localizedDescription }, { result in result.output})
+//        let message = self.onResult(branch)
+//        send(message)
+//    }
+//}
 
 struct Magit: Program {
     enum Message {
         case quit
-        case setBranch(String)
+        case setStatus(StatusEnum)
     }
 
     struct Model {
-        var branch = ""
+        var status: StatusEnum = .loading
     }
 
     func initial() -> (Model, [Command]) {
-        let getBranch = Cmd.run(runner: { branchName() }, onResult: { branchName in Message.setBranch(branchName)})
-        return (Model(), [getBranch])
+        //let getBranch = Cmd.run(process: branchName(), onResult: { branchName in Message.setBranch(branchName)})
+        let getStatus = Status(onResult: {result in Message.setStatus(result)})
+        return (Model(status: .loading), [getStatus])
     }
 
     func update(model: inout Model, message: Message) -> Update<Model> {
         switch message {
         case .quit:
             return .quit
-        case let .setBranch(branch):
-            model.branch = branch
+        case let .setStatus(status):
+            model.status = status
         }
         
         return .model(model)
     }
 
     func render(model: Model, in screenSize: Size) -> Component {
+        let components: [Component]
+        switch model.status {
+        case .loading:
+            components = [LabelView(at: .topLeft(), text: "Loading...")]
+        case .error(let error):
+            components = [LabelView(at: .topLeft(), text: error.localizedDescription)]
+        case .success(let status):
+            let untracked = status.changes.filter(isUntracked)
+            let unstaged = status.changes.filter(isUnstaged)
+            let staged = status.changes.filter(isStaged)
+            components = [
+                FlowLayout.vertical(size: DesiredSize(width: screenSize.width, height: screenSize.height), components: [
+                    Section(title: .LabelView(LabelView(text: "Head:     " + Text(status.branch, [.foreground(Color.cyan)]) + " " + status.log[0].message)), items: [], itemMapper: { $0 }, open: true, screenSize: screenSize),
+                    Section(title: .String("Untracked files (\(untracked.count))"), items: untracked, itemMapper: changeMapper, open: true, screenSize: screenSize),
+                    Section(title: .String("Unstaged changes (\(unstaged.count))"), items: unstaged, itemMapper: changeMapper, open: true, screenSize: screenSize),
+                    Section(title: .String("Staged changes (\(staged.count))"), items: staged, itemMapper: changeMapper, open: true, screenSize: screenSize),
+                    Section(title: .String("Recent commits"), items: status.log, itemMapper: commitMapper, open: true, screenSize: screenSize)
+                ])
+            ]
+        }
+        
         return Window(
-          components: [
-            LabelView(at: .topLeft(), text: "Head:\t\t\(model.branch)"),
-            OnKeyPress(.q, { Message.quit })
-          ]
+          components: components + [OnKeyPress(.q, {Message.quit})]
         )
     }
 }
+
+func commitMapper(_ commit: GitCommit) -> LabelView {
+    LabelView(text: Text(commit.hash.short, [.foreground(.any(241))]) + " \(commit.message)")
+}
+
+func changeMapper(_ change: Change) -> LabelView {
+    switch change.status {
+    case .Added:
+        return LabelView(text: "new file  \(change.file)")
+    case .Untracked:
+        return LabelView(text: change.file)
+    case .Modified:
+        return LabelView(text: "modified  \(change.file)")
+    case .Deleted:
+        return LabelView(text: "modified  \(change.file)")
+    case .Renamed:
+        return LabelView(text: "renamed   \(change.file)")
+    case .Copied:
+        return LabelView(text: "copied    \(change.file)")
+    }
+}
+
 
 let app = App(program: Magit(), screen: TermboxScreen())
 let exitState = app.run()
@@ -70,3 +112,4 @@ switch exitState {
 case .quit: exit(EX_OK)
 case .error: exit(EX_IOERR)
 }
+
---
+++ Sources/magit/status/StatusCommand.swift
@@ -0,0 +1,59 @@
+//
+//  StatusCommand.swift
+//  magit
+//
+//  Created by Bendik Solheim on 02/04/2020.
+//
+
+import Foundation
+import BowEffects
+import Ashen
+
+public enum StatusEnum {
+    case loading
+    case success(StatusInfo)
+    case error(Error)
+}
+
+public struct StatusInfo {
+    let branch: String
+    let changes: [Change]
+    let log: [GitCommit]
+}
+
+public class Status: Command {
+    
+    public typealias OnResult = (StatusEnum) -> AnyMessage
+    
+    let onResult: OnResult
+    
+    init(onResult: @escaping OnResult) {
+        self.onResult = onResult
+    }
+    
+    public func start(_ send: @escaping (AnyMessage) -> Void) {
+        let tasks = IO.parZip (execute(process: Git.branchName()),
+                               execute(process: Git.status()).flatMap({ result in parseStatus(result.output).fold(IO.raiseError, IO.pure) }),
+                               execute(process: Git.log(num: 10))
+            )^
+        let result = tasks.unsafeRunSyncEither()
+        let status: StatusEnum = result.fold(self.error, self.success)
+        let message = self.onResult(status)
+        send(message)
+    }
+    
+    func error(error: Error) -> StatusEnum {
+        return .error(error)
+    }
+    
+    func success(branch: ProcessResult, status: GitStatus, log: ProcessResult) -> StatusEnum {
+        let branch = branch.output
+        let log = parseCommits(log.output)
+        
+        return .success(StatusInfo(
+            branch: branch,
+            changes: status.changes,
+            log: log
+        ))
+    }
+}
