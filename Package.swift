f1dffc0cf791c6f3385015047bd51e31defedb10
Bendik Solheim <hello@bendik.dev>
2020-04-08 19:30:30 +0200

    Add Ashen and Bow to dependencies

--- Package.swift
+++ Package.swift
@@ -7,17 +7,17 @@ let package = Package(
     name: "magit",
     platforms: [ .macOS(.v10_14) ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
       // .package(url: "https://github.com/colinta/Ashen.git", .branch("master"))
-        .package(path: "../../github-external/Ashen")
+        .package(path: "../../github-external/Ashen"),
+        .package(url: "https://github.com/bow-swift/bow.git", from: "0.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "magit",
-            dependencies: ["Ashen"]),
+            dependencies: ["Ashen", "BowEffects"]),
         .testTarget(
             name: "magitTests",
             dependencies: ["magit"]),
