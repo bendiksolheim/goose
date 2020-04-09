f8b6b5402d1df65665aa528ff8ac7526a0bc2be8
Bendik Solheim <hello@bendik.dev>
2020-04-09 20:02:12 +0200

    More test cases

--- Tests/GitLibTests/gitLibTests.swift
+++ Tests/GitLibTests/gitLibTests.swift
@@ -5,16 +5,30 @@ import Bow
 final class changeParseTests: XCTestCase {
     
     func testOneUntrackedFile() {
-        let input = "? lol.md"
+        let input = "? file.md"
         let changes = parseChange(input).getOrElse([])
-        let expected = [Change(area: Area.Worktree, status: FileStatus.Untracked, file: "lol.md")]
+        let expected = [Change(area: .Worktree, status: .Untracked, file: "file.md")]
         XCTAssertEqual(changes, expected)
     }
     
     func testOneUnstagedFile() {
-        let input = "? lol.md"
+        let input = "1 .M N... 100644 100644 100644 d057de2786136730ee81a024345f1181a084138a d057de2786136730ee81a024345f1181a084138a file.md"
         let changes = parseChange(input).getOrElse([])
-        let expected = [Change(area: Area.Worktree, status: FileStatus.Untracked, file: "lol.md")]
+        let expected = [Change(area: .Worktree, status: .Modified, file: "file.md")]
+        XCTAssertEqual(changes, expected)
+    }
+    
+    func testOneStagedFile() {
+        let input = "1 M. N... 100644 100644 100644 d057de2786136730ee81a024345f1181a084138a f808ed536bda8d492749f2e4d12bc4ce74284e7a file.md"
+        let changes = parseChange(input).getOrElse([])
+        let expected = [Change(area: .Index, status: .Modified, file: "file.md")]
+        XCTAssertEqual(changes, expected)
+    }
+    
+    func testNewFileStagedThenDeletedInWorktree() {
+        let input = "1 AD N... 000000 100644 000000 0000000000000000000000000000000000000000 e69de29bb2d1d6434b8b29ae775ad8c2e48c5391 file.md"
+        let changes = parseChange(input).getOrElse([])
+        let expected = [Change(area: .Index, status: .Added, file: "file.md"), Change(area: .Worktree, status: .Deleted, file: "file.md")]
         XCTAssertEqual(changes, expected)
     }
 
