caa2665415b392ecc60a1174111365a263a5e5dc
Bendik Solheim <hello@bendik.dev>
2020-04-10 22:18:41 +0200

    Add system for switching views, add log view cases

--- Sources/magit/Magit.swift
+++ Sources/magit/Magit.swift
@@ -2,28 +2,47 @@ import Foundation
 import Ashen
 import os.log
 
+enum View {
+    case status
+    case log
+}
+
 struct Magit: Program {
     enum Message {
         case quit
         case setStatus(AsyncData<StatusInfo>)
+        case setLog(AsyncData<LogInfo>)
+        case viewLog
         case refresh
     }
 
     struct Model {
         var status: AsyncData<StatusInfo> = .loading
+        var log: AsyncData<LogInfo> = .loading
+        var views: [View]
     }
 
     func initial() -> (Model, [Command]) {
         let getStatus = Status(onResult: {result in Message.setStatus(result)})
-        return (Model(status: .loading), [getStatus])
+        return (Model(status: .loading, views: [.status]), [getStatus])
     }
 
     func update(model: inout Model, message: Message) -> Update<Model> {
         switch message {
         case .quit:
-            return .quit
+            if model.views.count > 1 {
+                model.views.removeLast()
+                return .update(model, [])
+            } else {
+                return .quit
+            }
         case .setStatus(let status):
             model.status = status
+        case .setLog(let log):
+            model.log = log
+        case .viewLog:
+            model.views.append(.log)
+            return .update(model, [Log(onResult: { Message.setLog($0) })])
         case .refresh:
             let (_, command) = initial()
             return .update(model, command)
@@ -33,10 +52,22 @@ struct Magit: Program {
     }
 
     func render(model: Model, in screenSize: Size) -> Component {
-        let components = renderStatus(status: model.status, screenSize: screenSize)
+        let view = model.views.last!
+        let components: [Component]
+        switch view {
+        case .status:
+            components = renderStatus(status: model.status, screenSize: screenSize)
+        case .log:
+            os_log("renderLog")
+            components = renderLog(log: model.log, screenSize: screenSize)
+        }
 
         return Window(
-            components: components + [OnKeyPress(.q, {Message.quit}), OnKeyPress(.g, { Message.refresh })]
+            components: components +
+                [OnKeyPress(.q, { Message.quit }),
+                 OnKeyPress(.g, { Message.refresh }),
+                 OnKeyPress(.l, { Message.viewLog })
+                ]
         )
     }
 }
