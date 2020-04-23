deac2188d3275c8f41db5448f879ce1224fe908f
Bendik Solheim <hello@bendik.dev>
2020-04-23 23:03:49 +0200

    Refactor update function for readability

--- Sources/goose/App/Goose.swift
+++ Sources/goose/App/Goose.swift
@@ -35,45 +35,57 @@ func update(message: Message, model: Model) -> (Model, Cmd<Message>) {
     switch message {
     case .gotStatus(let newStatus):
         return (model.copy(withStatus: StatusModel(info: newStatus, visibility: model.status.visibility)), .none)
+        
     case .gotLog(let log):
         return (model.copy(withLog: log), .none)
+        
     case .cursorUpdate(let x, let y):
         return (model.copy(withCursor: CursorModel(x, y)), .none)
+        
     case .keyboard(let event):
-        switch event {
-        case .q:
-            if model.views.count > 1 {
-                return (model.popView(), .none)
-            } else {
-                return (model, .exit)
-            }
-        case .l:
-            return (model.pushView(view: .log), task(getLog))
-        case .g:
-            return (model, task(getStatus))
-        case .c:
-            return (model, process(commit))
-        default:
-            return (model, .none)
-        }
+        return parseKey(event, model: model)
+        
     case .stage(let changes):
         if changes[0].area == .Worktree {
             return (model, task({ addFile(files: changes.map { $0.file }) }))
         } else {
             return (model, .cmd(.info("Already staged")))
         }
+        
     case .unstage(let changes):
         if changes[0].area == .Index {
             return (model, task({ resetFile(files: changes.map { $0.file }) }))
         } else {
             return (model, .cmd(.info("Already unstaged")))
         }
+        
     case .updateVisibility(let visibility):
         return (model.copy(withStatus: model.status.copy(withVisibility: visibility)), .none)
+        
     case .commandSuccess:
         return (model, task(getStatus))
+        
     case .info(let error):
         os_log("Info: %{public}@", error)
+        return (model, .none)
+    }
+}
+
+func parseKey(_ event: KeyEvent, model: Model) -> (Model, Cmd<Message>) {
+    switch event {
+    case .q:
+        if model.views.count > 1 {
+            return (model.popView(), .none)
+        } else {
+            return (model, .exit)
+        }
+    case .l:
+        return (model.pushView(view: .log), task(getLog))
+    case .g:
+        return (model, task(getStatus))
+    case .c:
+        return (model, process(commit))
+    default:
         return (model, .none)
     }
 }
