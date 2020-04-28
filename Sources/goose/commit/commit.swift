4b13a21e2af17c05435159167f595d08b0b17acb
Bendik Solheim <hello@bendik.dev>
2020-04-28 19:53:28 +0200

    Remove commented out code

--- Sources/goose/commit/commit.swift
+++ Sources/goose/commit/commit.swift
@@ -65,37 +65,6 @@ func runCommand(_ command: String) -> Int {
     return returnStatus
 }
 
-/*func withCStrings(_ strings: [String], scoped: ([UnsafeMutablePointer<CChar>?]) throws -> Void) rethrows {
-    let cStrings = strings.map { strdup($0) }
-    try scoped(cStrings + [nil])
-    cStrings.forEach { free($0) }
-}
-
-enum RunCommandError: Error {
-    case WaitPIDError
-    case POSIXSpawnError(Int32)
-}
-
-func runCommand(_ command: String, completion: ((Int32) -> Void)? = nil) throws {
-    var pid: pid_t = 0
-    let args = ["sh", "-c", command]
-    let envs = ProcessInfo().environment.map { k, v in "\(k)=\(v)" }
-    try withCStrings(args) { cArgs in
-        try withCStrings(envs) { cEnvs in
-            var status = posix_spawn(&pid, "/bin/sh", nil, nil, cArgs, cEnvs)
-            if status == 0 {
-                if (waitpid(pid, &status, 0) != -1) {
-                    completion?(status)
-                } else {
-                    throw RunCommandError.WaitPIDError
-                }
-            } else {
-                throw RunCommandError.POSIXSpawnError(status)
-            }
-        }
-    }
-}*/
-
 func _WSTATUS (_ x: CInt) -> CInt  { return x & 0x7F         }
 func WSTOPSIG (_ x: CInt) -> CInt  { return x >> 8           }
 func WIFEXITED(_ x: CInt) -> Bool  { return _WSTATUS(x) == 0 }
@@ -110,8 +79,6 @@ func WIFSIGNALED (_ x: CInt) -> Bool {
 
 func WEXITSTATUS(_ x: CInt) -> CInt { return (x >> 8) & 0xFF }
 func WTERMSIG   (_ x: CInt) -> CInt { return _WSTATUS(x) }
-        
-        
 
 private func currentDirectory() -> String {
     return FileManager.default.currentDirectoryPath
