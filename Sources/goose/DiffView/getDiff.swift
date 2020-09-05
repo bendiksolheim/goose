1dd01e46e725d1c1055cea6eb0ae57a475c2325f
Bendik Solheim <hello@bendik.dev>
2020-09-05 23:18:51 +0200

    Retain git command log in diff view

--- Sources/goose/DiffView/getDiff.swift
+++ Sources/goose/DiffView/getDiff.swift
@@ -5,17 +5,20 @@ import GitLib
 
 
 func getDiff(git: Git, _ ref: String) -> Message {
-    let commit = Task<GitCommit>.var()
-    let stats = Task<Stats>.var()
+    let commit = Task<ProcessResult>.var()
+    let stats = Task<ProcessResult>.var()
     let tasks = binding(
-        commit <- git.show.patch([ref]).exec().map { parseCommit(git: git)($0.output) },
-        stats <- git.show.stat(ref).exec().map { parseStat($0.output) },
-        yield: commitSuccess(commit.get, stats.get)
+        commit <- git.show.patch([ref]).exec(),
+        stats <- git.show.stat(ref).exec(),
+        yield: commitSuccess(git: git, commit.get, stats.get)
     )^
     
-    return .GitResult([], .GotCommit(ref, tasks.unsafeRunSyncEither().fold(error, identity)))
+    return runAndMap(tasks) { .GotCommit(ref, $0) }
 }
 
-private func commitSuccess(_ commit: GitCommit, _ stats: Stats) -> AsyncData<CommitInfo> {
-    .Success(CommitInfo(commit: commit, stats: stats))
+private func commitSuccess(git: Git, _ commit: ProcessResult, _ stats: ProcessResult) -> GitLogAndResult<AsyncData<CommitInfo>> {
+    GitLogAndResult(
+        [commit, stats].map { GitLogEntry($0) },
+        .Success(CommitInfo(commit: parseCommit(git: git)(commit.output), stats: parseStat(stats.output)))
+    )
 }
