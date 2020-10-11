6a2382f572aee6cae551b8564e272cd0280dcb40
Bendik Solheim <hello@bendik.dev>
2020-10-11 22:05:19 +0200

    Move git config function to where it is used

--- Sources/goose/Git/Config.swift
+++
@@ -1,8 +0,0 @@
-import GitLib
-import Bow
-import BowEffects
-
-func config(git: Git) -> Task<GitConfig> {
-    return git.config.all().exec()
-        .map { git.config.parse($0.output) }^
-}
--- Sources/goose/StatusView/getStatus.swift
+++ Sources/goose/StatusView/getStatus.swift
@@ -109,3 +109,8 @@ struct GitLogAndResult<T> {
         self.result = result
     }
 }
+
+func config(git: Git) -> Task<GitConfig> {
+    return git.config.all().exec()
+        .map { git.config.parse($0.output) }^
+}
