dcbf841e747f0f6c5628c79e9565bb28b5620deb
Bendik Solheim <hello@bendik.dev>
2021-04-14 21:53:50 +0200

    Remove log bloat

--- Sources/goose/util/AsyncData.swift
+++ Sources/goose/util/AsyncData.swift
@@ -1,5 +1,4 @@
 import Foundation
-import os.log
 
 public enum AsyncData<T: Equatable>: Equatable {
     case Loading
@@ -7,22 +6,17 @@ public enum AsyncData<T: Equatable>: Equatable {
     case Error(Error)
 
     public static func == (lhs: AsyncData<T>, rhs: AsyncData<T>) -> Bool {
-        os_log("lhs: (%{public}@), rhs: (%{public}@)", "\(lhs)", "\(rhs)")
         switch (lhs, rhs) {
         case (.Loading, .Loading):
-            os_log("loading")
             return true
 
         case let (.Success(l), .Success(r)):
-            os_log("success")
             return l == r
 
         case let (.Error(l), .Error(r)):
-            os_log("error")
             return l.localizedDescription == r.localizedDescription
 
         default:
-            os_log("different")
             return true
         }
     }
