0c1edf604f11c418830059bfb71ac5869577c6c2
Bendik Solheim <hello@bendik.dev>
2022-08-17 22:51:09 +0200

    Calculate relative commit age in log

--- Sources/goose/LogView/renderLog.swift
+++ Sources/goose/LogView/renderLog.swift
@@ -30,10 +30,31 @@ func renderCommit(_ commit: GitCommit) -> Node {
             { ref in " " + FormattedText("\(ref)", .Cyan) + " " + commit.message }
     )
     let message = Text(messageText, [(.enter, Message.GitCommand(.GetCommit(commit.hash.full)))], .Fill)
-    let author = Text(FormattedText(commit.author, .LightRed) + "   " + FormattedText("8 years"))
+    let author = Text(FormattedText(commit.author, .LightRed) + "   " + formatCommitDate(commit.commitDate))
     return Horizontal(.Fill, .Auto) {
         hash
         message
         author
     }
+}
+
+// Taken from https://github.com/magit/magit/blob/bf0ef3826bcda9d90f3e3c9a8f801c2c3c01bc5b/lisp/magit-margin.el#L204
+let seconds: [(String, String, Double)] = [
+    ("year", "years", (60 * 60 * 24 * 365.2425).rounded()),
+    ("month", "months", (60 * 60 * 24 * 30.436875).rounded()),
+    ("week", "weeks", (60 * 60 * 24 * 7)),
+    ("day", "days", (60 * 60 * 24)),
+    ("hour", "hours", (60 * 60)),
+    ("minute", "minutes", 60),
+    ("second", "seconds", 1)
+]
+
+func formatCommitDate(_ date: Date) -> String {
+    let interval = Date().timeIntervalSince(date)
+    let (unit, units, seconds) = seconds.first { (unit, units, seconds) in
+        interval > seconds
+    }!
+
+    let value = Int((interval / seconds).rounded())
+    return "\(value) \(value > 1 ? units : unit)"
 }
