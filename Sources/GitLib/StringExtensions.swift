ef2bd47e53c2e186f1f0fefd261fd74f3dc11c2b
Bendik Solheim <hello@bendik.dev>
2022-08-20 21:55:43 +0200

    Add lines() extenson to string

--- Sources/GitLib/StringExtensions.swift
+++ Sources/GitLib/StringExtensions.swift
@@ -16,6 +16,14 @@ public extension String {
         let ranges = [startIndex ..< startIndex] + matches.map { Range($0.range, in: self)! } + [endIndex ..< endIndex]
         return (0 ... matches.count).map { String(self[ranges[$0].upperBound ..< ranges[$0 + 1].lowerBound]) }
     }
+
+    func lines() -> [String] {
+        if self == "" {
+            return []
+        } else {
+            return split(regex: "\n")
+        }
+    }
 }
 
 private extension String {
