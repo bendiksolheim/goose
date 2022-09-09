bc2fd9c9c572eaa4d89fc14ba752b476ec9e084a
Bendik Solheim <hello@bendik.dev>
2022-09-09 21:08:51 +0200

    Use new subscription system

--- Sources/goose/App/Subscription.swift
+++ Sources/goose/App/Subscription.swift
@@ -2,10 +2,6 @@ import Foundation
 import Tea
 
 let subscriptions: [Sub<Message>] = [
-    .Keyboard { event in
-        .TerminalEvent(.Keyboard(event))
-    },
-    .Cursor { event in
-        .TerminalEvent(.Cursor(event))
-    }
+    Keyboard.onKeyPress { .TerminalEvent(.Keyboard($0)) },
+    Cursor.onMove { .TerminalEvent(.Cursor($0))}
 ]
